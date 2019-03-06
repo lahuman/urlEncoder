c11c633dcd0eb0894cd8095ceaa26c4686dcc319
LIM KWANG KYU <kk.lim@gsshop.com>
2019-03-06 18:05:19 +0900

    update regex LastIndex postion

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
 const uencoder = require('./lib/urlencoder');
 
 console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('http://gjtnghks.godohosting.com/섬유/포장안내/칼린%20하단_사방넷.jpg'));
+console.log(uencoder('https://shop-phinf.pstatic.net/20181218_172/101333752_1545130921098VDklP_GIF/%B0%ED%B8%E0%B3%D7%C4%DA_%C0%DA%BC%AE.gif'));
+console.log(uencoder('https://shop-phinf.pstatic.net/20181218_172/101333752_1545130921098VDklP_GIF/%B0%ED%B8%E0%B3%D7%C4%DA_%C0%DA%BC%AE.gif'));
+console.log(uencoder('https://shop-phinf.pstatic.net/20181218_172/101333752_1545130921098VDklP_GIF/%B0%ED%B8%E0%B3%D7%C4%DA_%C0%DA%BC%AE.gif'));
+console.log(uencoder('https://shop-phinf.pstatic.net/20181218_172/101333752_1545130921098VDklP_GIF/%B0%ED%B8%E0%B3%D7%C4%DA_%C0%DA%BC%AE.gif'));
 console.log(uencoder('https://shop-phinf.pstatic.net/20181218_172/101333752_1545130921098VDklP_GIF/%B0%ED%B8%E0%B3%D7%C4%DA_%C0%DA%BC%AE.gif'));
